b5e55ee69c6f2a7cec2f9866fdab9d2a33d28f3b
jemgdev <jemgdev@gmail.com>
2023-06-24 22:10:18 -0500

    Add new endpoint to validate an accessToken

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { loginController, getNewAccessToken } from '../controllers/auth.controller'
+import { loginController, getNewAccessToken, loginByAccessToken } from '../controllers/auth.controller'
 import isAuthenticated from '../middlewares/isAuthenticated'
 const authRouter = Router()
 
 authRouter.post('/login', loginController)
+authRouter.post('/login-token', loginByAccessToken)
 authRouter.get('/refresh-token', getNewAccessToken)
 authRouter.get('/protected-data', isAuthenticated, (request, response) => {
   response.json({
