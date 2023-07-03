e6c13285ac283618cc209f6ef41616eaa7379cc6
jemgdev <jemgdev@gmail.com>
2023-07-03 10:38:11 -0500

    Add inital data and add code field of project table

---
+++ initial-data.sql
@@ -0,0 +1,51 @@
+INSERT INTO farmer_quality VALUES
+(1, "Bueno"),
+(2, "Regular"),
+(3, "Malo"),
+(4, "Pésimo"),
+(5, "No registra");
+
+INSERT INTO property_legal_condition VALUES
+(1, "Adjudicado"),
+(2, "Arrendatario"),
+(3, "Conducc"),
+(4, "Posesión"),
+(5, "Titulado");
+
+INSERT INTO sector VALUES
+(4, "Margen izquierda"),
+(3, "Tumbes"),
+(2, "Margen derecha");
+
+INSERT INTO project VALUES
+(1, "El Palmar", 2, 1),
+(2, "Cooperativa 20 de Enero", 2, 2),
+(3, "Romero", 2, 3),
+(4, "La Tuna", 2, 4),
+(5, "Puerto El Cura", 2, 5),
+(6, "Las Brujas Alta", 2, 6),
+(7, "Las Brujas Baja", 2, 7),
+(8, "Becerra Belén", 2, 8),
+(9, "Coop. Ntra. Sra. del Carmen", 2, 9),
+(10, "Bebedero", 2, 10),
+(11, "Huaquillas", 2, 11),
+(12, "El Limon", 2, 12),
+(13, "La Palma", 2, 13),
+(14, "Papayal", 2, 14),
+(15, "Pocitos", 2, 15),
+(16, "Matapalo", 2, 16),
+(17, "Tumbes", 3, 1),
+(18, "Higuerón", 4, 2),
+(19, "San Julián", 4, 3),
+(20, "Casablanqueada", 4, 4),
+(21, "Oidor", 4, 5),
+(22, "Cristales", 4, 6),
+(23, "La Noria", 4, 7),
+(24, "Malvales", 4, 8),
+(25, "Realengal", 4, 9),
+(26, "La Cruz", 4, 10),
+(27, "Coop 5 de Diciembre", 4, 11),
+(28, "Rica Playa", 3, 15),
+(29, "La Rosita", 3, 16),
+(30, "Monteo", 3, 17),
+(31, "La Paila", 3, 18);
